c8ecfa74327399609558ae8b28ae70d61763d7e0
AdheetMarathe <adheet.marathe@gmail.com>
2025-08-13 09:48:40 -0700

    Add files via upload

--- Initials/src/Main.java
+++ Initials/src/Main.java
@@ -3,79 +3,26 @@ import kareltherobot.*;
 public class Main implements Directions{
   public static void main(String[] args) {
     World.setVisible(true);// allows us to see the run
-    World.setDelay(3);
-    World.setSize(12,12);
+    World.setSize(9,9);
+    World.setDelay(10);
 
     // The line below creates a Robot that we will refer to as r.  
     // Find out what the numbers and direction do!
-    Robot r = new Robot(1,1,East,900);
+    Robot r = new Robot(1,4,East,90);
 		
     // examples of commands you can invoke on a Robot
-  // move one step in the direction it is facing
+    r.move();// move one step in the direction it is facing
     r.turnLeft();
     r.putBeeper();
     r.move();
-	r.putBeeper();
-  r.move();
-  r.putBeeper();
-  r.move();
-  r.putBeeper();
-r.move();
-r.putBeeper();
+r.turnLeft();
 r.move();
 r.putBeeper();
 r.turnLeft();
 r.turnLeft();
 r.turnLeft();
 r.move();
-r.putBeeper();
-r.move();
-r.putBeeper();
-r.move();
-r.putBeeper();
 r.turnLeft();
-r.turnLeft();
-r.turnLeft();
-r.move();
-r.putBeeper();
-r.move();
-r.putBeeper();
-r.turnLeft();
-r.turnLeft();
-r.turnLeft();
-r.move();
-r.putBeeper();
-r.move();
-r.putBeeper();
-r.move();
-r.putBeeper();
-r.turnLeft();
-r.turnLeft();
-r.move();
-r.move();
-r.move();
-r.turnLeft();
-r.turnLeft();
-r.turnLeft();
-r.move();
-r.putBeeper();
-r.move();
-r.putBeeper();
-r.move();
-r.putBeeper();
-r.turnLeft();
-r.move();
-r.move();
-r.turnLeft();
-r.putBeeper();
-r.move();
-r.putBeeper();
-r.move();
-r.putBeeper();
-r.move();
-r.putBeeper();
-r.move();
-r.putBeeper();
 r.move();
 r.putBeeper();
 r.turnLeft();
@@ -83,11 +30,24 @@ r.turnLeft();
 r.turnLeft();
 r.move();
 r.turnLeft();
+r.move();
+r.putBeeper();
 r.turnLeft();
+r.turnLeft();
+r.turnLeft();
+r.move();
+r.turnLeft();
+r.move();
+r.putBeeper();
+r.turnLeft();
+r.turnLeft();
+r.move();
 r.turnLeft();
 r.move();
 r.putBeeper();
 r.move();
+r.turnLeft();
+r.turnLeft();
 r.turnLeft();
 r.move();
 r.putBeeper();
@@ -101,19 +61,50 @@ r.turnLeft();
 r.turnLeft();
 r.move();
 r.putBeeper();
+r.move();
 r.turnLeft();
 r.turnLeft();
 r.turnLeft();
 r.move();
 r.putBeeper();
 r.move();
-r.putBeeper();
+r.turnLeft();
 r.move();
 r.putBeeper();
 r.move();
-r.putBeeper();
+r.turnLeft();
+r.turnLeft();
+r.turnLeft();
 r.move();
 r.putBeeper();
+r.move();
+r.turnLeft();
+r.move();
+r.putBeeper();
+r.turnLeft();
+r.turnLeft();
+r.turnLeft();
+r.move();
+r.turnLeft();
+r.turnLeft();
+r.turnLeft();
+r.move();
+r.putBeeper();
+r.turnLeft();
+r.move();
+r.turnLeft();
+r.turnLeft();
+r.turnLeft();
+r.move();
+r.putBeeper();
+r.move();
+r.turnLeft();
+r.move();
+r.putBeeper();
+r.move();
+
+
+
 
   }
 }
